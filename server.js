5b5f66fcd55bd3ae7b14a473c0b3868fd0db43be
novak <monarchtechcorp@gmail.com>
2017-11-14 11:49:52 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+
+app.set('view engine','hbs');
+
+hbs.registerHelper('getCurrentYear', () => {return new Date().getFullYear()});
+
+hbs.registerHelper('screamIt', (text) => {return text.toUpperCase()});
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    //backtick with variable referenced
+    var log =`${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log',log +'\n', (err) => {
+    if (err) {
+        console.log('log error');
+    }
+    });
+    next();
+});
+
+//app.use((req, res, next) => {
+//   res.render('maintenance.hbs',{
+//       pageTitle: 'Maint'
+//   }); });
+
+app.get('/',(req,res) => {
+   res.render('home.hbs',{
+       pageTitle: 'Home Page',
+       text:'hi man'
+   }); 
+});
+app.use(express.static(__dirname + '/public'));
+app.get('/about',(req,res) =>{
+   res.render('about.hbs',{
+       pageTitle: 'About Page'
+   }); 
+})
+
+
+app.get('/bad',(req,res) =>{
+   res.send({error:'this is an error'}); 
+})
+
+app.listen(3000,() =>{
+    console.log('server is up on port 3000');
+});
