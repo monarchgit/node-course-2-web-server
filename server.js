e536381c583221ffa042d2f7e08681ffa44defd9
novak <monarchtechcorp@gmail.com>
2017-11-16 14:00:09 -0600

    added projects

--- server.js
+++ server.js
@@ -46,6 +46,11 @@ app.get('/about',(req,res) =>{
    }); 
 })
 
+app.get('/projects',(req,res) =>{
+   res.render('projects.hbs',{
+       pageTitle: 'Projects Page'
+   }); 
+})
 
 app.get('/bad',(req,res) =>{
    res.send({error:'this is an error'}); 
